4d391220cd4d63483e21662abda09012f2c2f6bc
François Nogueira <franznogueira51@gmail.com>
2024-07-16 23:42:11 -0300

    Alteração no Projeto caneta

--- caneta/App.java
+++ caneta/App.java
@@ -1,6 +1,12 @@
 public class App {
-    public void main(String []args){
-        System.out.println("Olá, mundo!");
+    public static void main(String []args){
+        Caneta c = new Caneta("Faber",0.8f,"azul");
+        c.status();
+        System.out.println("-----------------");
+        Caneta c1 = new Caneta("BIC", 0.8F, "Preto");
+        c1.status();
+         
+        
     }
 
 }

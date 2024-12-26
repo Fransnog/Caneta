aab24050876bcd9201e5bc8029b9e160251269c2
François Nogueira <141291059+Fransnog@users.noreply.github.com>
2024-12-26 01:46:18 -0300

    Add files via upload

---
+++ caneta/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String []args){
+        Caneta c = new Caneta("Faber",0.8f,"azul");
+        c.status();
+        System.out.println("-----------------");
+        Caneta c1 = new Caneta("BIC", 0.8F, "Preto");
+        c1.status();
+         
+        
+    }
+
+}
